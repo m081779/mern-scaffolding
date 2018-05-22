9d2a766a3bdbbb6be5468b99c3e343021bff0eec
m081779 <marcoprincipio@hotmail.com>
2018-05-22 00:48:05 -0400

    fixing some linter errors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const database = require('./config/database')
+const database = require('./config/database');
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
